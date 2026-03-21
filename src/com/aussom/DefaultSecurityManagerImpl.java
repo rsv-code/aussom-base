5dbb64890fc1b3366defa2848d0f1b5751188fb8
Austin Lehman <cup_of_code@fastmail.com>
2026-03-21 00:12:55 -0700

    A little code cleanup.

--- src/com/aussom/DefaultSecurityManagerImpl.java
+++ src/com/aussom/DefaultSecurityManagerImpl.java
@@ -8,8 +8,5 @@ public class DefaultSecurityManagerImpl extends SecurityManagerImpl {
          */
         this.props.put("aussomdoc.file.getJson", true);
         this.props.put("aussomdoc.class.getJson", true);
-
-        // TODO: Remove this, it's just for dev testing.
-        this.props.put("test.aussom.runner", true);
     }
 }
